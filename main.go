d69d7903f4b9d88fca3b1017beae2a932a343105
pingpong71 <term.vague0w@icloud.com>
2024-04-09 22:23:09 -0700

    fixes based on feedback

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 // The purpose of this program is to open a list of users
 // and return all the repos the user has that are public via the
 // github api.
@@ -16,133 +17,130 @@ import (
 	"strings"
 )
 
-func print_repos(body []byte) {
-// take the response body and parse for print_repos
-// TODO: filter out repose that were forked so we only scan repos we created
+func printRepos(body []byte) {
+	// take the response body and parse for print_repos
+	// TODO: filter out repose that were forked so we only scan repos we created
 
-    var json_data []map[string]interface{}
-    err := json.Unmarshal(body, &json_data)
-    if err != nil {
-        log.Println("Error:", err)
-    }
+	var json_data []map[string]string
+	err := json.Unmarshal(body, &json_data)
+	if err != nil {
+		log.Println("Error:", err)
+	}
 
-    for _, object := range json_data {
-        fmt.Println(object["full_name"])
-    }
+	for _, object := range json_data {
+		fmt.Println(object["full_name"])
+	}
 }
 
-func has_repos(body []byte) bool {
-// make sure the user we are checking on has repos
-// if they do not github returns a message like 
-// {"message": "Not Found"}
+func hasRepos(body []byte) bool {
+	// make sure the user we are checking on has repos
+	// if they do not github returns a message like
+	// {"message": "Not Found"}
 
-    var message map[string]interface{}
-    err := json.Unmarshal(body, &message)
-    if err != nil {
-       return true 
-    }
-
-    return false
+	var message map[string]interface{}
+	err := json.Unmarshal(body, &message)
+	return err != nil
 }
 
-func get_token() (string, error) {
-// load token file off disk
-// TODO: put token into a secure store
+func getToken() (string, error) {
+	// load token file off disk
+	// TODO: put token into a secure store
 
-    homedir, err := os.UserHomeDir()
-    if err != nil {
-        log.Fatal(err)
-    }
-    file, err := os.Open(homedir + "/.github_token")
-    if err != nil {
-        log.Fatal("Unable to open token file")
-    }
-    defer file.Close()
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Println(err)
+	}
+	file, err := os.Open(homedir + "/.github_token")
+	if err != nil {
+		log.Println("Unable to open token file")
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        return scanner.Text(), nil
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		return scanner.Text(), nil
+	}
 
-    return "", errors.New("empty token") 
+	return "", errors.New("empty token")
 }
 
-func make_request(url string) *http.Response {
-// make a get request to github api to do something
-    
-    token, err := get_token()
-    if err != nil {
-        log.Fatal("Token error:", err)
-    }
-    req, err := http.NewRequest("GET", url, nil)
-    req.Header.Add("Authorization", token)
-    client := &http.Client{}
+func makeRequest(url string) *http.Response {
+	// make a get request to github api to do something
 
-    resp, err := client.Do(req)
-    if err != nil {
-        log.Println("Error", err)
-    }
+	token, err := getToken()
+	if err != nil {
+		log.Println("Token error:", err)
+		return nil
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("Error:", err)
+	}
+	req.Header.Add("Authorization", token)
+	client := &http.Client{}
 
-    return resp
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error", err)
+	}
+
+	return resp
 }
 
-func next_page(link []string) (string, bool) {
-// github api does pagination so we need to handle that
-// https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28
+func nextPage(link []string) (string, bool) {
+	// github api does pagination so we need to handle that
+	// https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28
 
-    link_map := map[string]string{}
-    for _, item := range strings.Split(link[0], ",") {
-        re := regexp.MustCompile(`\<(.*)\>\;\s+rel\=\"(.*)\"`)
-        match := re.FindStringSubmatch(item)
-        link_map[match[2]] = match[1]
-    }
-    
-    if _, exists := link_map["next"]; exists {
-        return link_map["next"], true
-    }
+	link_map := map[string]string{}
+	for _, item := range strings.Split(link[0], ",") {
+		re := regexp.MustCompile(`\<(.*)\>\;\s+rel\=\"(.*)\"`)
+		match := re.FindStringSubmatch(item)
+		link_map[match[2]] = match[1]
+	}
 
-    return "", false
+	if _, exists := link_map["next"]; exists {
+		return link_map["next"], true
+	}
+
+	return "", false
 }
 
 func main() {
-    url := "https://api.github.com/users/"
+	url := "https://api.github.com/users/"
 
-    file, err := os.Open("users.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
+	file, err := os.Open("users.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        user := scanner.Text()
-        resp := make_request(url + user + "/repos")
-        defer resp.Body.Close()
-        for {
-            body, err := io.ReadAll(resp.Body)
-            if err != nil {
-                log.Println("Error:", err)
-            }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		user := scanner.Text()
+		resp := makeRequest(url + user + "/repos")
+		defer resp.Body.Close()
+		for {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("Error:", err)
+			}
 
-            if has_repos(body) {
-                print_repos(body)
-            }
+			if hasRepos(body) {
+				printRepos(body)
+			}
 
-            var next string
-            var do_next bool
-            if len(resp.Header["Link"]) > 0 {
-                next, do_next = next_page(resp.Header["Link"])
-            }
+			var next string
+			var do_next bool
+			if len(resp.Header["Link"]) > 0 {
+				next, do_next = nextPage(resp.Header["Link"])
+			}
 
-            if !do_next {
-                break
-            }
+			if !do_next {
+				break
+			}
 
-            resp = make_request(next)
-        }
+			resp = makeRequest(next)
+		}
 
-    }
+	}
 }
-
-
-
-
